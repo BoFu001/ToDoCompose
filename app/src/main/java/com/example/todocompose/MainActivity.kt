2b2c15e6f3c7ab0a228105239925b38275351663
Bo Fu <bofu2013@gmail.com>
2022-05-05 17:57:01 +0200

    Added constraintLayout in compose

--- app/src/main/java/com/example/todocompose/MainActivity.kt
+++ app/src/main/java/com/example/todocompose/MainActivity.kt
@@ -3,13 +3,15 @@ package com.example.todocompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.constraintlayout.compose.ChainStyle
+import androidx.constraintlayout.compose.ConstraintLayout
 import com.example.todocompose.ui.theme.ToDoComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,9 +19,54 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ToDoComposeTheme {
-
+                TestConstraintLayout()
             }
         }
+    }
+}
+
+@Composable
+fun TestConstraintLayout(){
+    ConstraintLayout (
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Red)
+    ) {
+
+
+        val (btn1, btn2, btn3) = createRefs()
+        val guidelineFromBottom = createGuidelineFromBottom(0.1f)
+
+        Button(
+            onClick = { println("aaaaa btn1")},
+            modifier = Modifier.constrainAs(btn1){
+                bottom.linkTo(guidelineFromBottom)
+            }
+        ){
+            Text(text = "Btn1")
+        }
+
+        Button(
+            onClick = { println("aaaaa btn2")},
+            modifier = Modifier.constrainAs(btn2){
+                bottom.linkTo(parent.bottom)
+            }
+        ){
+            Text(text = "Btn2")
+        }
+
+        Button(
+            onClick = { println("aaaaa btn3")},
+            modifier = Modifier.constrainAs(btn3){
+                bottom.linkTo(parent.bottom)
+            }
+        ){
+            Text(text = "Btn3")
+        }
+
+        createHorizontalChain(btn1, btn2, btn3, chainStyle = ChainStyle.Spread)
+
+
     }
 }
 
